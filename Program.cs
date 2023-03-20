a9a0fc6e8d24ece8e29f3dc2381fb5d5bca670aa
Henry Brookland (1000112868) <BROOHA1@student.op.ac.nz>
2023-03-21 11:58:12 +1300

    Add in files to get set up

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Heavenly_Visions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
